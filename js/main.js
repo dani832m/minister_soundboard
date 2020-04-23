2ce375d189a417c8da7a6de913ba979bb459da59
Daniel Lyck <d9@live.dk>
2020-04-24 00:09:03 +0200

    Sounds added and code optimized.

--- js/main.js
+++ js/main.js
@@ -6,3 +6,27 @@ window.addEventListener(
   },
   false
 );
+
+/* Play audio */
+const playAudio = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio) return; // If no audio, stop running
+  audio.currentTime = 0; // Rewind to the start
+  audio.play();
+  key.classList.add("playing");
+};
+
+/* Remove transition */
+const removeTransition = (e) => {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+};
+
+/* Call 'removeTransition' on every key when transition ends */
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+/* Listen on keys */
+window.addEventListener("keydown", playAudio);
